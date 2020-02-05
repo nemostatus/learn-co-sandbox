1101c7a47e598df2b626ba6ae8fb07ca1291cf35
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-05 01:13:23 +0000

    cli change

---
+++ starships_cli/Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in starships_cli.gemspec
+gemspec
---
+++ starships_cli/lib/starships_cli.rb
@@ -0,0 +1,5 @@
+require_relative "./starships_cli/version"
+require_relative "./starships_cli/api"
+require_relative "./starships_cli/cli"
+require_relative "./starships_cli/starship"
+
