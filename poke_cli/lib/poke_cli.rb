532694ea0b04c0eb1423b08cd7c35dd43a315c18
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-10-19 01:43:18 +0000

    Automatically backed up by Learn

--- poke_cli/lib/poke_cli.rb
+++
@@ -1,9 +0,0 @@
-require_relative "./poke_cli/version"
-require_relative "./poke_cli/api"
-require_relative "./poke_cli/cli"
-require_relative "./poke_cli/pokemon"
-require "rest-client"
-require "json"
-
-
-
